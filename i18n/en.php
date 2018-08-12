fcdf76ce7425a98afaefac85536908c00ffbe4c0
Xaektide <saektide@saektide.com>
2018-08-12 13:16:38 -0300

    +help +changelog

--- i18n/en.php
+++ i18n/en.php
@@ -13,7 +13,7 @@ $i_FAQ = 'FAQ';
 
 $i_AddWiki = 'Add wiki';
 
-$i_unAuthedInfo = 'Oh, hello. We just upgraded the security here. You can join only if you received an auth key. Do you have it?';
+$i_unAuthedInfo = 'Oh, hello. We just upgraded the security here. You can join only if you received an auth key. Do you have it? If you want access, you should contact to Saektide (Saektide#4411 / saektide@saektide.com).';
 $i_unAuthedExit = 'Nope';
 $i_unAuthedJoin = 'Yes';
 $i_unAuthedInvalid = 'Make sure it\'s a valid key.';
--- i18n/es.php
+++ i18n/es.php
@@ -13,7 +13,7 @@ $i_FAQ = 'FAQ';
 
 $i_AddWiki = 'Añadir wiki';
 
-$i_unAuthedInfo = 'Oh, hola. Hemos actualizado la seguridad aquí. Puedes entrar solo si has recibido una clave de autenticación. ¿La tienes?';
+$i_unAuthedInfo = 'Oh, hola. Hemos actualizado la seguridad aquí. Puedes entrar solo si has recibido una clave de autenticación. ¿La tienes? Si deseas el acceso, deberías contactarte con Saektide (Saektide#4411 / saektide@saektide.com).';
 $i_unAuthedExit = 'No';
 $i_unAuthedJoin = 'Si';
 $i_unAuthedInvalid = 'Asegurate de que es una clave valida.';
